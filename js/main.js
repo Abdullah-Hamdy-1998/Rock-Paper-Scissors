c39bc6ccd8cb1dd3032f2ec6c8b323ee382fe1f0
Abdullah Hamdy <abdullah.hamdi.1998@gmail.com>
2024-12-30 16:52:30 +0200

    Add function to generate computer's choice

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 let humanScore = 0;
 let computerScore = 0;
 
+function getComputerChoice() {
+  let randomNumber = Math.floor(Math.random() * 3);
+  switch (randomNumber) {
+    case 0:
+      return 'rock';
+    case 1:
+      return 'paper';
+    case 2:
+      return 'scissors';
+  }
+}
