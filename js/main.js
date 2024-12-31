0e27f54adfced7673e4e429050ba54fdc041d3a3
Abdullah Hamdy <abdullah.hamdi.1998@gmail.com>
2024-12-31 16:02:08 +0200

    Refactor user input validation and fix formatting in main.js

--- js/main.js
+++ js/main.js
@@ -5,19 +5,24 @@ function getComputerChoice() {
   let randomNumber = Math.floor(Math.random() * 3);
   switch (randomNumber) {
     case 0:
-      return 'rock';
+      return "rock";
     case 1:
-      return 'paper';
+      return "paper";
     case 2:
-      return 'scissors';
+      return "scissors";
   }
 }
 
 function getHumanChoice() {
-  let humanChoice = prompt('Please choose either rock, paper, or scissors.');
-  if (humanChoice === 'rock' || humanChoice === 'paper' || humanChoice === 'scissors') {
+  let humanChoice = prompt("Please choose either rock, paper, or scissors.");
+  if (
+    humanChoice === "rock" ||
+    humanChoice === "paper" ||
+    humanChoice === "scissors"
+  ) {
     return humanChoice;
   } else {
-    alert('You entered an invalid option. Please try again.');
+    alert("You entered an invalid option. Please try again.");
+    return getHumanChoice();
   }
 }
